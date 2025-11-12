c5fd03fb5988891dbc6dd0dc44ca75e3ea164767
sarensabesk <sarensabeskal1@gmail.com>
2025-11-12 16:39:40 -0500

    Helper Functions for ingest.py

--- sentiment-analyzer/src/ingest.py
+++ sentiment-analyzer/src/ingest.py
@@ -14,3 +14,25 @@ import feedparser
 #All Downloaded CSV go here
 DATA_DIR = os.path.join(os.path.dirname(__file__), "..", "data")
 os.makedirs(DATA_DIR, exist_ok=True)
+
+
+def cache_to_csv(df, name):
+    
+    if not name.endswith(".csv"): #if the file doesnt end with csv, add it to the end
+        name = name + ".csv"
+    
+    path = os.path.join(DATA_DIR, name) #adds the file into data folder
+    
+    df.to_csv(path, index=False) #creates csv in path
+    
+    return os.path.abspath(path) ## converts to absolute path
+
+
+def _as_date(x):
+   return pd.to_datetime(x).date() # removes time to just YYYY-MM-DD
+    
+
+def _clean_text(s):
+    if not s:
+        return ""
+    return re.sub(r"\s+", " ", s).strip()
