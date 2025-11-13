97e4279d4755923c298f0dfe9b78e4f6a216c0d5
sarensabesk <sarensabeskal1@gmail.com>
2025-11-12 20:03:28 -0500

    Finish core_headlines

--- sentiment-analyzer/src/nlp.py
+++ sentiment-analyzer/src/nlp.py
@@ -18,11 +18,10 @@ def get_sia():
     
     global _sia
 
-    if _sia is not None:
+    if _sia is not None: #if variable has a value dont change nth
         return _sia
     
-    _sia = SentimentIntensityAnalyzer()
-
+    #if it doesnt have that value, then give it that value (sia)
     try:
         _sia = SentimentIntensityAnalyzer()
     
@@ -31,8 +30,37 @@ def get_sia():
         nltk.download("vader_lexicon")
         _sia = SentimentIntensityAnalyzer()
 
-    
     return _sia
+
+def score_headlines(df):
+    #checks to see if valid 
+
+    required = {"Date", "Headline"}
+    
+    if not required.issubset(df.columns):
+        missing = required - set(df.columns)
+        raise ValueError(f"Missing required columns: {missing}")
+
+   
+    if df.empty:
+        return df.copy()
+    
+    analyzer = get_sia()
+
+    texts = df["Headline"].fillna("").astype(str)
+
+    scores_series = texts.apply(analyzer.polarity_scores) # Series of dicts
+    scores_df = pd.DataFrame(scores_series.tolist())  # columns: neg, neu, pos, compound
+
+    out = df.copy()
+    out[["pos", "neu", 'neg', 'compound']] = scores_df[["pos", "neu", 'neg', 'compound']]
+
+    return out
+
+
+
+
+
 
 
 
